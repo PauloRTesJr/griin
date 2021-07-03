c412baecf3d19a17f4ff6811d706ba8f9a70eb16
Paulo Tessarolli <paulortesjr@gmail.com>
2021-07-03 14:18:22 -0300

    initial commit

---
+++ apps/griin-backoffice/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'griin-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'griin-backoffice';
+}
