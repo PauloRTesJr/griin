8bcd3b64842ae5c4bc4fcebfc5ac23948cc17f29
Paulo Tessarolli <paulortesjr@gmail.com>
2021-07-17 16:29:59 -0300

    Remove unused import

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonComponent } from './components/button/button.component';
 import { InputComponent } from './components/input/input.component';
 import { IconComponent } from './components/icon/icon.component';
