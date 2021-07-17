0852cfa8dca005fd1f7b63dd204be48001fe7bf4
Paulo Tessarolli <paulortesjr@gmail.com>
2021-07-17 10:27:46 -0300

    Trying fix Problem with builder

--- libs/ui/src/lib/components/input/input.component.spec.ts
+++ libs/ui/src/lib/components/input/input.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { InputComponent } from './input.component';
 
@@ -8,9 +9,9 @@ describe('InputComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InputComponent ]
-    })
-    .compileComponents();
+      imports: [ReactiveFormsModule],
+      declarations: [InputComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
