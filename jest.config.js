8582ba4c1591ebd678a5354fe9c6bc53074d414d
Paulo Tessarolli <paulo.roberto@opus-software.com.br>
2021-07-23 13:01:07 -0300

    Nx Update

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
+    ...getJestProjects(),
     '<rootDir>/apps/griin-backoffice',
     '<rootDir>/libs/ui',
     '<rootDir>/libs/features/user',
